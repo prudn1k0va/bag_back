67aa9a20538dae3519ac49fc2d7571f55bd0c28c
prudn1k0va <aristvodolaz@mail.ru>
2022-07-14 17:12:49 +0300

    2 com

--- bag.cpp
+++ bag.cpp
@@ -7,6 +7,15 @@ int knapsack1(const std::vector<int>& wts, const std::vector<int>& cost, int W)
 	std::vector<int> dp(W + 1);
 	dp[0] = 0;
 	for (int w = 1; w <= W; w++)
-
+	{
+		dp[w] = dp[w - 1];
+		for (size_t i = 0; i < n; i++)
+		{
+			if (wts[i] <= w)
+			{
+				dp[w] = std::max(dp[w], dp[w - wts[i]] + cost[i]);
+			}
+		}
+	}
 	return dp[W];
 }
